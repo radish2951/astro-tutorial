7647da8d6f784ac5337beb2b131ffd17b3b243f3
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2024-03-01 19:54:33 +0900

    Revert "ViewTransitionsを有効"
    This reverts commit e82c5cb6198d872113a09531efead1c83de38cad.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,3 @@
-document.addEventListener('astro:page-load', () => {
-    document.querySelector('.hamburger').addEventListener('click', () => {
-        document.querySelector('.nav-links').classList.toggle('expanded');
-    });
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
 });
